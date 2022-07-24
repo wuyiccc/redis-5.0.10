ea5bec480e34158259cf655a9b184a77ed3aa899
wuyiccc <3406324191@qq.com>
2022-07-25 07:23:19 +0800

    插入节点

--- src/t_zset.c
+++ src/t_zset.c
@@ -139,53 +139,72 @@ int zslRandomLevel(void) {
  * exist (up to the caller to enforce that). The skiplist takes ownership
  * of the passed SDS string 'ele'. */
 zskiplistNode *zslInsert(zskiplist *zsl, double score, sds ele) {
+    // 记录每层被插入节点的前一个节点
     zskiplistNode *update[ZSKIPLIST_MAXLEVEL], *x;
+    // 计算每层到update节点的span
     unsigned int rank[ZSKIPLIST_MAXLEVEL];
     int i, level;
 
     serverAssert(!isnan(score));
+    // 头
     x = zsl->header;
+    // 从顶层遍历
     for (i = zsl->level-1; i >= 0; i--) {
         /* store rank that is crossed to reach the insert position */
+        // 如果不是顶层, 则i层的起始rank值为上层rank
         rank[i] = i == (zsl->level-1) ? 0 : rank[i+1];
         while (x->level[i].forward &&
                 (x->level[i].forward->score < score ||
                     (x->level[i].forward->score == score &&
                     sdscmp(x->level[i].forward->ele,ele) < 0)))
         {
+            // 本层rank累加
             rank[i] += x->level[i].span;
             x = x->level[i].forward;
         }
+        // 记录搜索节点
         update[i] = x;
     }
     /* we assume the element is not already inside, since we allow duplicated
      * scores, reinserting the same element should never happen since the
      * caller of zslInsert() should test in the hash table if the element is
      * already inside or not. */
+    // 获取随机level
     level = zslRandomLevel();
+    // 如果获取的level大于层高
     if (level > zsl->level) {
+        // 处理高于层高的节点
         for (i = zsl->level; i < level; i++) {
             rank[i] = 0;
+            // 经过节点为头节点
             update[i] = zsl->header;
+            // 头节点span为跳跃表长度
             update[i]->level[i].span = zsl->length;
         }
         zsl->level = level;
     }
+    // 创建新节点
     x = zslCreateNode(level,score,ele);
     for (i = 0; i < level; i++) {
+        // 依次设置新节点的forward
         x->level[i].forward = update[i]->level[i].forward;
+        // 将沿途记录的各个节点的forward指针指向新节点
         update[i]->level[i].forward = x;
 
         /* update span covered by update[i] as x is inserted here */
+        // 计算新节点的跨度
         x->level[i].span = update[i]->level[i].span - (rank[0] - rank[i]);
+        // 更新沿途的span值
         update[i]->level[i].span = (rank[0] - rank[i]) + 1;
     }
 
     /* increment span for untouched levels */
+    // 未接触的节点span+1
     for (i = level; i < zsl->level; i++) {
         update[i]->level[i].span++;
     }
 
+    // 设置backward
     x->backward = (update[0] == zsl->header) ? NULL : update[0];
     if (x->level[0].forward)
         x->level[0].forward->backward = x;
